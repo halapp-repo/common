e3595fbe043a3165e543f3afaff8c4bc60b6a931
Furkan Ozdeslik <furkan.ozdeslik@gmail.com>
2023-01-18 07:15:44 +0300

    update code

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,14 @@ import { AddressVM } from "./models/viewmodels/address.viewmodel";
 import { UserVM } from "./models/viewmodels/user.viewmodel";
 import { OrganizationVM } from "./models/viewmodels/organization.viewmodel";
 import { InventoryVM } from "./models/viewmodels/inventory.viewmodel";
+import { PriceVM } from "./models/viewmodels/price.viewmodel";
 
-export { OrderVM, OrderItemVM, AddressVM, UserVM, OrganizationVM, InventoryVM };
+export {
+  OrderVM,
+  OrderItemVM,
+  AddressVM,
+  UserVM,
+  OrganizationVM,
+  InventoryVM,
+  PriceVM,
+};
